6541ad4067d25b422ee93afa47dfed0b9adeaff9
tomnycui <tomnycui@126.com>
2024-06-06 22:12:49 +0800

    implanting intrinstc

--- bin/semantics.ml
+++ bin/semantics.ml
@@ -1,7 +1,106 @@
-module Check = struct
-  (* open Syntactics.AST
+module Util = struct
+  let report msg rng =
+    Printf.eprintf "StructureError: \"%s\" at %s\n" msg
+      (Lexical.Range.to_string rng)
+end
 
-  let rec check (e : t) : bool =
-    match e with Atom (_, _) -> true
-    | List _ -> () *)
+(** [Env] is the semantic environment *)
+module Env = struct
+  type t = (string * Typing.AST.t) list
+
+  let find name t : Typing.AST.t option =
+    match List.find_opt (fun p -> fst p = name) t with
+    | Some n -> Some (snd n)
+    | None -> None
+
+  let insert (name, node) t : t = (name, node) :: t
+end
+
+(** [StaticCheck] module implants 
+    the static check mechanism adopted from original LISP paper. *)
+module StaticCheck = struct
+  exception VarNotDefined of string * Lexical.Range.t
+
+  type env = Env.t
+  type vartype = Typing.MachineType.t
+
+  module A = Syntactics.AST
+  open Typing.AST
+
+  let rec check_with_env (raw_ast : A.t) env : t * env =
+    match raw_ast with
+    | A.Atom (name, range) -> (
+        match Typing.smart_atom name with
+        | Atom (T_term s) ->
+            let found =
+              match Env.find s env with
+              | Some v -> v
+              | None ->
+                  Util.report "VarNotDefined" range;
+                  Atom T_err
+            in
+            (found, env)
+        | _ as a -> (a, env))
+    (* quote *)
+    | A.List ([ Atom ("quote", _); e ], _) ->
+        (List [ Intrinsic I_quote; hold_cnv e ], env)
+    | A.List (Atom ("quote", _) :: _, range) ->
+        Util.report "quote only allow one argument" range;
+        (Atom T_err, env)
+    (* atom *)
+    | A.List ([ Atom ("atom", _); e1 ], _) ->
+        let s, env = check_with_env e1 env in
+        (List [ Intrinsic I_atom; s ], env)
+    | A.List (Atom ("atom", _) :: _, range) ->
+        Util.report "atom only allow one argument" range;
+        (Atom T_err, env)
+    (* eq *)
+    | A.List ([ Atom ("eq", _); e1; e2 ], _) ->
+        let s1, env = check_with_env e1 env in
+        let s2, env = check_with_env e2 env in
+        (List [ Intrinsic I_eq; s1; s2 ], env)
+    | A.List (Atom ("eq", _) :: _, range) ->
+        Util.report "eq only allow two arguments" range;
+        (Atom T_err, env)
+    (* car *)
+    | A.List ([ Atom ("car", _); e1 ], _) ->
+        let s, env = check_with_env e1 env in
+        (List [ Intrinsic I_car; s ], env)
+    | A.List (Atom ("car", _) :: _, range) ->
+        Util.report "car only allow one argument" range;
+        (Atom T_err, env)
+    (* cdr *)
+    | A.List ([ Atom ("cdr", _); e1 ], _) ->
+        let s, env = check_with_env e1 env in
+        (List [ Intrinsic I_cdr; s ], env)
+    | A.List (Atom ("cdr", _) :: _, range) ->
+        Util.report "cdr only allow one argument" range;
+        (Atom T_err, env)
+    (* cons *)
+    | A.List ([ Atom ("cons", _); e1; e2 ], _) ->
+        let s1, env = check_with_env e1 env in
+        let s2, env = check_with_env e2 env in
+        (List [ Intrinsic I_cons; s1; s2 ], env)
+    | A.List (Atom ("cons", _) :: _, range) ->
+        Util.report "cons only allow two arguments" range;
+        (Atom T_err, env)
+    (* cond *)
+    | _ -> (Atom T_unit, env)
+
+  and hold_cnv e =
+    match e with
+    | Atom (name, _) -> Typing.smart_atom name
+    | List (lst, _) -> List (List.map hold_cnv lst)
+
+  let check_one (raw_ast : A.t) = check_with_env raw_ast []
+
+  let check_list (raw_ast_list : A.t list) : t list * env =
+    let rec loop acc l e =
+      match l with
+      | [] -> (List.rev acc, e)
+      | hd :: tl ->
+          let app, e' = check_with_env hd e in
+          loop (app :: acc) tl e'
+    in
+    loop [] raw_ast_list []
 end
--- bin/typing.ml
+++ bin/typing.ml
@@ -1,7 +1,68 @@
+(** [MachineType] types are only type that have a basic 
+      machine representation. These types can be represented 
+      directly by machine on some storage material like memory. 
+      And thus they can only be constructed by some intrinsic
+      constructors, and can only be manipulated with some 
+      builtin functions. 
+
+      All [MachineType] types are "constructed" from [T_term],
+      and can be "destructed" to [T_term].
+  *)
+module MachineType = struct
+  type t =
+    (* err, to introduce type error *)
+    | T_err
+    (* unit, should equiv to empty list *)
+    | T_unit
+    (* unconstructed id type *)
+    | T_term of string
+    (* interger types *)
+    | T_i32 of int32
+    | T_i64 of int64
+    (* floating point types *)
+    | T_f64 of float
+    (* string *)
+    | T_str of string
+    (* char *)
+    | T_char of char
+    (* byte sequence *)
+    | T_bytes of bytes
+    (* boolean *)
+    | T_bool of bool
+
+  (* constructors *)
+  let ctor_i32 s =
+    match Int32.of_string_opt s with
+    | Some i -> T_i32 i
+    | None -> T_unit
+
+  let ctor_i64 s =
+    match Int64.of_string_opt s with
+    | Some i -> T_i64 i
+    | None -> T_unit
+
+  let ctor_f64 s =
+    match Float.of_string_opt s with
+    | Some i -> T_f64 i
+    | None -> T_unit
+
+  let ctor_str s = T_str s
+
+  let ctor_char s =
+    match String.length s with 1 -> T_char s.[0] | _ -> T_unit
+
+  let ctor_bytes s = T_bytes (String.to_bytes s)
+
+  let ctor_bool = function
+    | "true" -> T_bool true
+    | "false" -> T_bool false
+    | _ -> T_unit
+end
+
 (** [Typing.AST] is the true internal high order AST. 
     And since in LISP, type is equiv to quoted list, so AST is type. *)
 module AST = struct
-  type atom = Native.MachineType.t
+  type atom = MachineType.t
 
   type intrinstc =
     (* from original paper *)
@@ -15,14 +76,14 @@ module AST = struct
     | I_lambda
     | I_label
     | I_defun
-    (* extended subst *)
-    | I_alpha (* alpha reduction*)
-    (* extended cond *)
-    | I_if (* cond sugar *)
+    (* extended subst, alpha reduction *)
+    | I_alpha
+    (* extended cond, cond sugar *)
+    | I_if
     (* list sugar *)
     | I_list
-    (* append takes two lists
-       and returns their concatenation *)
+      (* append takes two lists
+         and returns their concatenation *)
     | I_append
     (* pair takes two lists of the same length
        and returns a list of two element lists
@@ -46,6 +107,7 @@ module AST = struct
     | I_ceil
     | I_pow
     | I_sqrt
+    | I_log
     (* boolean and compare *)
     | I_true
     | I_false
@@ -55,13 +117,77 @@ module AST = struct
     | I_bor
     | I_lor
     | I_xor
+    | I_lt
+    | I_gt
+    | I_leq
+    | I_geq
+    | I_eeq
     (* machine *)
     | I_shl
     | I_shr
     | I_lshr
+    (* machine type constructor
+       unit need no constructor, it is empty list *)
+    | C_i32
+    | C_i64
+    | C_f64
+    | C_str
+    | C_char
+    | C_bytes
 
   (** The ast type, this one won't preserve position information,
       which should be discarded in precession of static type 
-      checking and [Syntactis.AST] -> [Typing.AST] translating. *)
+      checking and [Syntactis.AST] -> [Typing.AST] translating. 
+      
+      However, keywords and type construct is done at this part,
+      making it possible to implant macro. *)
   type t = Atom of atom | Intrinsic of intrinstc | List of t list
 end
+
+let smart_atom name : AST.t =
+  match name with
+  | "quote" -> Intrinsic I_quote
+  | "atom" -> Intrinsic I_atom
+  | "eq" -> Intrinsic I_eq
+  | "car" -> Intrinsic I_car
+  | "cdr" -> Intrinsic I_cdr
+  | "cons" -> Intrinsic I_cons
+  | "cond" -> Intrinsic I_cond
+  | "lambda" -> Intrinsic I_lambda
+  | "label" -> Intrinsic I_label
+  | "defun" -> Intrinsic I_defun
+  | "alpha" -> Intrinsic I_alpha
+  | "if" -> Intrinsic I_if
+  | "list" -> Intrinsic I_list
+  | "append" -> Intrinsic I_append
+  | "pair" -> Intrinsic I_pair
+  | "assoc" -> Intrinsic I_assoc
+  | "neg" -> Intrinsic I_neg
+  | "add" -> Intrinsic I_add
+  | "sub" -> Intrinsic I_sub
+  | "mul" -> Intrinsic I_mul
+  | "div" -> Intrinsic I_div
+  | "mod" -> Intrinsic I_mod
+  | "round" -> Intrinsic I_round
+  | "floot" -> Intrinsic I_floot
+  | "ceil" -> Intrinsic I_ceil
+  | "pow" -> Intrinsic I_pow
+  | "sqrt" -> Intrinsic I_sqrt
+  | "log" -> Intrinsic I_log
+  | "true" -> Intrinsic I_true
+  | "false" -> Intrinsic I_false
+  | "not" -> Intrinsic I_not
+  | "band" -> Intrinsic I_band
+  | "land" -> Intrinsic I_land
+  | "bor" -> Intrinsic I_bor
+  | "lor" -> Intrinsic I_lor
+  | "xor" -> Intrinsic I_xor
+  | "lt" -> Intrinsic I_lt
+  | "gt" -> Intrinsic I_gt
+  | "leq" -> Intrinsic I_leq
+  | "geq" -> Intrinsic I_geq
+  | "eeq" -> Intrinsic I_eeq
+  | "shl" -> Intrinsic I_shl
+  | "shr" -> Intrinsic I_shr
+  | "lshr" -> Intrinsic I_lshr
+  | _ as s -> Atom (T_term s)
